d0f5d7d5b95e9d8a54175dd6f9a69547b37c532f
JS-Genius <1000ssnake@gmail.com>
2016-05-01 14:43:01 -0500

    Core File

--- coredata/core.js
+++ coredata/core.js
@@ -28,7 +28,7 @@ var screen = {
       height: document.getElementById("game").height,
       draw: document.getElementById("game").getContext("2d"),
       keys: [],
-      mouse: {z
+      mouse: {
             x: 0,
             y: 0,
             click: function() {},
